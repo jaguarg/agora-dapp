3e44807ff2948cd0b9d8d001a06c4d3c42b17fff
m0rpheus <fotshudi@gmail.com>
2023-07-09 21:27:55 +0200

    sconification

--- sconify.sh
+++ sconify.sh
@@ -17,7 +17,7 @@ docker pull registry.scontain.com:5050/sconecuratedimages/node:16.13.1-alpine3.1
 # run the sconifier to build the TEE image based on the non-TEE image
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.3.15 \
+            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.7.5-v8 \
             sconify_iexec \
             --name=${IMG_NAME} \
             --from=${IMG_FROM} \
