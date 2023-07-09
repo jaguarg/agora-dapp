4dff4ee5b8ab6a3aba861d0089461ebb4202429b
m0rpheus <fotshudi@gmail.com>
2023-07-09 23:40:32 +0200

    sconification

--- sconify.sh
+++ sconify.sh
@@ -15,23 +15,23 @@ docker build . -t ${IMG_FROM} --no-cache --progress=plain
 docker pull registry.scontain.com:5050/sconecuratedimages/node:16.13.1-alpine3.15
 
 # run the sconifier to build the TEE image based on the non-TEE image
-#docker run -it --rm \
-#            -v /var/run/docker.sock:/var/run/docker.sock \
-#            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.7.5-v8 \
-#            sconify_iexec \
-#            --name=${IMG_NAME} \
-#            --from=${IMG_FROM} \
-#            --to=${IMG_TO} \
-#            --binary-fs \
-#            --fs-dir=/app \
-#            --host-path=/etc/hosts \
-#            --host-path=/etc/resolv.conf \
-#            --binary=/usr/local/bin/node \
-#            --heap=1G \
-#            --dlopen=2 \
-#            --no-color \
-#            --verbose \
-#            --command=${ENTRYPOINT} \
-#            && echo -e "\n------------------\n" \
-#            && echo "successfully built TEE docker image => ${IMG_TO}" \
-#            && echo "application mrenclave.fingerprint is $(docker run -it --rm -e SCONE_HASH=1 ${IMG_TO})"
+docker run -it --rm \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.7.5-v8 \
+            sconify_iexec \
+            --name=${IMG_NAME} \
+            --from=${IMG_FROM} \
+            --to=${IMG_TO} \
+            --binary-fs \
+            --fs-dir=/app \
+            --host-path=/etc/hosts \
+            --host-path=/etc/resolv.conf \
+            --binary=/usr/local/bin/node \
+            --heap=1G \
+            --dlopen=2 \
+            --no-color \
+            --verbose \
+            --command=${ENTRYPOINT} \
+            && echo -e "\n------------------\n" \
+            && echo "successfully built TEE docker image => ${IMG_TO}" \
+            && echo "application mrenclave.fingerprint is $(docker run -it --rm -e SCONE_HASH=1 ${IMG_TO})"
